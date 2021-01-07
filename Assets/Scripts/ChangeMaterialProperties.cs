64f6243286f80d3926019109f725e9fe5a4e44a5
Lucas <lucas.chaudeau@iut-tarbes.fr>
2021-01-07 10:00:34 +0100

    city glass update for change color in game

--- Assets/Scripts/ChangeMaterialProperties.cs
+++ Assets/Scripts/ChangeMaterialProperties.cs
@@ -3,9 +3,10 @@
 // Change material and emissive
 public class ChangeMaterialProperties : MonoBehaviour
 {
+	[SerializeField] private int _materialIndex = 0;
 	private Material _objectMaterial = null;
 
-	private void Awake() => _objectMaterial = GetComponent<MeshRenderer>().materials[0];
+	private void Awake() => _objectMaterial = GetComponent<MeshRenderer>().materials[_materialIndex];
 
 	public void ChangeColor(Color color) => _objectMaterial.SetColor("_Color", color);
 }
