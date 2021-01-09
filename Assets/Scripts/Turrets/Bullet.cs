e73e87558dcb9d9d00088c4575ee8c17707aa87e
Wesley Petit <ptitwes.45@gmail.com>
2021-01-09 20:09:12 +0100

    Enemy Particles

--- Assets/Scripts/Turrets/Bullet.cs
+++ Assets/Scripts/Turrets/Bullet.cs
@@ -7,13 +7,17 @@ public class Bullet : MonoBehaviour, ICollidable
 	[SerializeField] private float _offScreen = 30f;                // Add to pool in a certain offset
 	[SerializeField] private Transform _heading = null;             // GFX to change rotation
 	[SerializeField] private Explosion _explosionPrefab = null;     // Explosion
+	[SerializeField]
+	private BulletParticles[] _bulletParticles      // Let particle when bullet his alive 
+		= new BulletParticles[0];
 
 	public TurretController Turret { get; set; }
 	public Rigidbody2D Rigidbody2D => _rb2D;
 
 	private bool IsOffScreen => _offScreen < transform.position.magnitude;
 
-	private bool _alreadyHit = false;								// Launch explosion only on the first collision
+	private bool _alreadyHit = false;                               // Launch explosion only on the first collision
+	private BulletParticles _currentBulletParticle = null;
 
 	private void Update()
 	{
@@ -40,6 +44,8 @@ public class Bullet : MonoBehaviour, ICollidable
 		_heading.rotation = rotationToDirection;
 		// Reset Hit
 		_alreadyHit = false;
+
+		LinkWithParticles();
 	}
 
 	public void ResetPhysics()
@@ -48,10 +54,30 @@ public class Bullet : MonoBehaviour, ICollidable
 		_rb2D.angularVelocity = 0f;
 	}
 
+	// Select one particle which have no particle / inactive
+	private void LinkWithParticles()
+	{
+		foreach (var particle in _bulletParticles)
+		{
+			if (particle.ParticleCount <= 0)
+			{
+				_currentBulletParticle = particle;
+				_currentBulletParticle.Link();
+				_currentBulletParticle.gameObject.SetActive(true);
+				break;
+			}
+		}
+	}
+
 	// OffScreen or Hit
 	private void AddToPool()
 	{
 		ResetPhysics();
+
+		// Let particle alive
+		if (_currentBulletParticle)
+			_currentBulletParticle.Unlink();
+
 		gameObject.SetActive(false);
 		Turret.AddBullet(this);
 	}
---
+++ Assets/Scripts/Turrets/BulletParticles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// Manage particle to let them alive when a bullet is destroy
+[RequireComponent(typeof(ParticleSystem))]
+public class BulletParticles : MonoBehaviour
+{
+	[SerializeField] private ParticleSystem _particles = null;
+	[SerializeField] private Transform _particleParent = null;                       // Transform which contains particle
+
+	public int ParticleCount => _particles.particleCount;
+
+	// Position particles in his original position
+	public void Link()
+	{
+		transform.SetParent(_particleParent, false);
+		_particles.Play(true);
+	}
+
+	// Loacl position to world position
+	public void Unlink()
+	{
+		transform.SetParent(null, false);
+		_particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+	}
+}
