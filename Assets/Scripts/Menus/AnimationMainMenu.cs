386873276b3c92557586c59f0d5c26700a1cdb58
Rémi Duplé <remi.duple@iut-tarbes.fr>
2021-01-18 01:57:00 +0100

    Modify the main title and adding some VR animations

---
+++ Assets/Scripts/Menus/AnimationMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimationMainMenu : MonoBehaviour
+{
+    private GameObject _vrCamera;
+    void Start()
+    {
+        DOTween.Init();
+        _vrCamera=GameObject.Find("VR Camera");
+    }
+
+    void Update()
+    {
+      gameObject.transform.DOLocalRotate(new Vector3(_vrCamera.transform.rotation.x, _vrCamera.transform.rotation.y, _vrCamera.transform.rotation.z), 0.5f);
+    }
+}
