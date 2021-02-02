5443a0581b643ad3f7c4447691ad8fe7d6205400
Rémi DUPLE <remi.duple@iut-tarbes.fr>
2021-02-02 12:10:49 +0100

    Adding corrections to timer UI

--- Assets/RemaningTimer.cs
+++ Assets/RemaningTimer.cs
@@ -18,18 +18,22 @@ public class RemaningTimer : MonoBehaviour
         float tempTime=RoundSystem.Instance.TotalTimeRounds/20f;
         timeRound=0.02f/tempTime;
         LineRenderer ownLine=this.GetComponent<LineRenderer>();
-        float CameraWidth=m_FirstCamera.pixelWidth;
-        float CameraHeight=m_FirstCamera.pixelHeight;
+        float CameraWidth=m_FirstCamera.pixelWidth/2;
+        float CameraHeight=m_FirstCamera.pixelHeight/2;
         ownLine.SetPosition(0, new Vector3(CameraWidth*-6.45f/901, CameraHeight*4.18f/584,0f));
         ownLine.SetPosition(1, new Vector3(CameraWidth*-6.45f/901, CameraHeight*-4.21f/584, 0f));
         ownLine.SetPosition(2, new Vector3(CameraWidth*6.45f/901, CameraHeight*-4.21f/584, 0f));
         ownLine.SetPosition(3, new Vector3(CameraWidth*6.45f/901, CameraHeight*4.18f/584, 0f));
         ownLine.SetPosition(4, new Vector3(CameraWidth*-6.45f/901, CameraHeight*4.18f/584, 0f));
+        ownLine.sortingOrder = 1;
+        ownLine.material = new Material (Shader.Find ("Sprites/Default"));
+        ownLine.material.color = Color.red; 
         decreaseSize();
     }
     void decreaseSize()
     {
         LineRenderer ownLine=this.GetComponent<LineRenderer>();
+        
         if(ownLine.GetPosition(0).y<=-4.1f || phaseOne)
         {
             phaseOne=true;
